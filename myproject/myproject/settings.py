e9befc10c89c3794fe9deffa73a37b431bb646c5
BlazingDiamond <kiangoing0604@gmail.com>
2025-11-21 06:06:23 +0200

    small hot fix

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -27,13 +27,13 @@ SECRET_KEY = "django-insecure-4k4$^djxjjn0oraphxg6ra5sca^4c(&no7^iy@2lqr4xj=@7-^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    "13.50.171.93",  # deployed back end ip
-    "192.168.0.21",
-    "yourdomain.com",
-    ".amazonaws.com",
-]
-
+# ALLOWED_HOSTS = [
+#     "13.50.171.93",  # deployed back end ip
+#     "192.168.0.21",
+#     "yourdomain.com",
+#     ".amazonaws.com",
+# ]
+Allowed_Hosts = ["*"]
 
 # Application definition
 
